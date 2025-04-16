7c02953ad803b2cbe07e87d3a518e6a065a99bfe
mohhamed hamdy <mohammedhammdy32@gmail.com>
2025-04-16 12:39:03 +0200

    Add new rassbrypi code

--- rass/mqtt.py
+++ rass/mqtt.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
+import random
 
+c = 0
 broker = "localhost"
 
 data_topic    = "home/data"
@@ -9,12 +11,28 @@ rass_control_topic = "home/rass_control"
 def on_message(client, userdata, message):
 
     print(f"Received message: {message.payload.decode()} on topic {message.topic}")
+
+    if message.payload.decode().startswith('Node'):
+       # Extract data form message
+       node_part, led_part = message.payload.decode().split(',')
+       node_number = node_part.split(':')[0].replace('Node', '')  # 2
+       value = node_part.split(':')[1]                            # 0.00
+       led_status = led_part.split(':')[1]                        # OFF
+       print(f"Node Number: {node_number}, Value: {value}, LED Status: {led_status}")
+
     # Put data in database
     ###
 
     # Send data
-    client.publish(rass_control_topic,"ON")
+    c = random.randint(0, 1)
+    #if c == 0:
+    #    client.publish(rass_control_topic,"Node2:LED:OFF\n")
+    #else:    
+    #    client.publish(rass_control_topic,"Node2:LED:OFF\n")
+    
+    print("------------------------------------------")
 
+c = 0
 client = mqtt.Client()
 client.connect(broker, 1883)
 client.subscribe(data_topic)
